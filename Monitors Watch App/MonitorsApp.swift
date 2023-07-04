ac9094fa2ca11c1647c23db506129e77d46a2d1c
Ben Shabowski <benshabowski@gmail.com>
2023-07-04 09:07:20 -0500

    Starting work on watch app

---
+++ Monitors Watch App/MonitorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorsApp.swift
+//  Monitors Watch App
+//
+//  Created by Benjamin Shabowski on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Monitors_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
