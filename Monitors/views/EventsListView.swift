a24f310adaf815f62c930f969c1d436266ffd233
Ben Shabowski <benshabowski@gmail.com>
2023-08-25 11:26:37 -0500

    Adding date pickers for a time range for events

--- Monitors/views/EventsListView.swift
+++ Monitors/views/EventsListView.swift
@@ -9,33 +9,43 @@ import SwiftUI
 
 struct EventsListView: View {
     @State var events: [Events] = []
-    @State var startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())
-    @State var endData = Date()
+    @State var startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+    @State var endDate = Date()
     
     var body: some View {
         NavigationStack {
-            Group {
+            HStack {
+                Spacer()
+                DatePicker("", selection: $startDate, in: ...endDate, displayedComponents: [.date])
+                Spacer()
+                Text(" to ")
+                Spacer()
+                DatePicker("", selection: $endDate, in: startDate..., displayedComponents: [.date])
+                Spacer()
+            }.labelsHidden()
+            List {
                 if(events.isEmpty){
-                    Text("No events found in the time frame given").padding()
+                    Text("No events found in the time frame given")
                 } else {
-                    List {
-                        ForEach(events, id: \.self.time){events in
-                            Section(formatDate(date: events.time)){
-                                ForEach(events.events, id:\.self.monitor){monitor in
-                                    Text("\(monitor.monitor) \(monitor.status ? "came up" : "went down")").foregroundColor(monitor.status ? Color.green : Color.red)
-                                }
+                    ForEach(events, id: \.self.time){events in
+                        Section(formatDate(date: events.time)){
+                            ForEach(events.events, id:\.self.monitor){monitor in
+                                Text("\(monitor.monitor) \(monitor.status ? "came up" : "went down")").foregroundColor(monitor.status ? Color.green : Color.red)
                             }
                         }
-                    }.refreshable {
-                        await refresh()
                     }
                 }
+            }
+            .refreshable {
+                await refresh()
             }
             .navigationTitle("Events")
         }
         .task {
             await refresh()
         }
+        .onChange(of: startDate, perform: {_ in Task {await refresh()}})
+        .onChange(of: endDate, perform: {_ in Task {await refresh()}})
     }
     
     func formatDate(date: Date) -> String {
@@ -46,7 +56,7 @@ struct EventsListView: View {
     
     func refresh() async {
         do {
-            events = try await fetch(startDate: startDate, endDate: endData)
+            events = try await fetch(startDate: startDate, endDate: endDate)
         } catch networkError.inavlidURL {
             print("u")
         } catch networkError.invalidData {
